576bc366d835e5bffd864cf5113f23b60b781adc
Anderson Nascimento dos Santos <tec.anderson.ns@gmail.com>
2023-05-01 07:53:19 -0300

    feat: setup experimental nextjs feature

--- next.config.js
+++ next.config.js
@@ -1,8 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withImages = require("next-images");
+
+module.exports = withImages({
+  webpack(config, options) {
+    return config;
+  },
+
+  // This line enables the dangerouslyAllowSVG option
   experimental: {
+    // optimizeImages: true,
+    // optimizeCss: true,
+    // removeUnusedCss: true,
+    // modern: true,
+    scrollRestoration: true,
+
+    // reactRoot: "root",
+    // concurrentFeatures: true,
+    dangerouslyAllowSVG: true,
+    // svg
     appDir: true,
   },
-}
-
-module.exports = nextConfig
+  images: {
+    domains: ["tailwindui.com"],
+  },
+});
